1d96cedeeea263707f6f00507580e97d42de860d
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-22 18:40:37 -0500

    Add update_post_counter and recent_comments for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
   has_many :likes
+
+  def update_post_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
