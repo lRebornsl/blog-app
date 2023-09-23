b6ceff2b3cb6fe537a136c102f300701c7c180d6
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-23 12:16:10 -0500

    Add like validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_create :update_like_counter
 
   private
