954416e1de0d0a53753086ce91ddd30e2c0ef5b7
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-23 12:15:43 -0500

    Add comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_create :update_comment_counter
 
   private
