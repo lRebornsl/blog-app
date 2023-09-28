5a57da50be137e72d1c2faf40434387b5a90d474
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-28 13:41:55 -0500

    Add optional code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :users do
-    get 'posts', to: 'posts#index', on: :member, as: 'posts'
-    get 'posts/:id', to: 'posts#show', on: :member, as: 'post'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
-  
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
 end
