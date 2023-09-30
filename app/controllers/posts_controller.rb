6ea28ede815d09b39f103d5833c98180bd055078
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-30 14:54:00 -0500

    Add view for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+  end
 
-  def show; end
+  def show
+    @post = Post.find_by(id: params[:id])
+
+    if @post.nil?
+      redirect_to posts_path
+    end
+  end
 end
