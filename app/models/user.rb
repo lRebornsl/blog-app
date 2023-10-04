3cd6cfef4030a6e69e87c9e68fd01fd6e1f3e9ad
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-04 17:17:37 -0500

    Add name field to register new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,13 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_initialize :set_default
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def set_default
+    self.posts_counter ||= 0
   end
 end
